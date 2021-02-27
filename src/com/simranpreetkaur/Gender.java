4c331a1c5dc3ccb14aafb320b019c6a5b33b10e0
Simranpreetkaur <harsim610@gmail.com>
2021-02-27 09:51:41 -0500

    Student class updated with all attributes.

---
+++ src/com/simranpreetkaur/Gender.java
@@ -0,0 +1,7 @@
+package com.simranpreetkaur;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHERS
+}
---
+++ src/com/simranpreetkaur/Student.java
@@ -0,0 +1,20 @@
+package com.simranpreetkaur;
+
+import java.util.Date;
+
+public class Student {
+    private int id;
+    private String first_name;
+    private String last_name;
+    private Date birth_date;
+    private Gender gender;
+    private String address;
+    private String city;
+    private String cell_phone;
+    private String email;
+    private String department;
+    private float marks[];
+    private float total_marks;
+    private float percentage;
+    private String grade;
+}
