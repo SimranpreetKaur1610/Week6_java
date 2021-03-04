65c557d2e14e87a2374951fd1cde7b078584d048
Simranpreetkaur <harsim610@gmail.com>
2021-03-04 14:05:07 -0500

    getter and setter

--- src/com/simranpreetkaur/Week06Sample.java
+++ src/com/simranpreetkaur/Week06Sample.java
@@ -1,7 +1,17 @@
 package com.simranpreetkaur;
 
+import java.util.Date;
+
 public class Week06Sample {
     public static void main(String [] args)    {
-
+        float[] dalveerMarks = {80,80,80,80,80};
+        Student dalveer = new Student(789821, "Dalveer", "Singh", new Date(), Gender.MALE, "String address", "String city", "String cellPhone", "String email", "String department", dalveerMarks);
+        dalveer.calculateGrades();
+        float[] dwarkeshMarks = {90,90,90,90,90};
+        Student dwarkesh = new Student(789822, "Dwarkesh", "Singh", new Date(), Gender.MALE, "String address", "String city", "String pinCode", "String cellPhone", "String email", "String department", dwarkeshMarks);
+        dalveer.calculateGrades();
+        dwarkesh.calculateGrades();
+        System.out.println("Dalveer Grades: "+dalveer.getGrade());
+        System.out.println("Dwarkesh Grades: "+dwarkesh.getGrade());
     }
 }
